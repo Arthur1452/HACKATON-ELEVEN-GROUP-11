731889c3cee190d7b63564c5122b64d1eb5e61a9
ilan <ouzenilan@yahoo.com>
2025-09-09 12:35:07 +0200

    premier commit

---
+++ doc.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd 
+
+import seaborn as sns 
+
+
+
+data=pd.read_csv("C:/Users/ouzen/Documents/HACKATON-ELEVEN-GROUP-11/waiting_times_train.csv")
+
+data.head()
+
